b7c3f3d340c82c08b2680432ccd1ff644e92a927
julody <julody@mail.ru>
2021-12-03 03:00:19 +0300

    front: start app implementing

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,24 +1,38 @@
 import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { TuiRootModule, TuiDialogModule, TuiNotificationsModule, TUI_SANITIZER } from "@taiga-ui/core";
+import {
+    TuiRootModule,
+    TuiDialogModule,
+    TuiNotificationsModule,
+    TUI_SANITIZER,
+    TuiGroupModule,
+    TuiTextfieldControllerModule, TuiHintControllerModule, TuiButtonModule
+} from "@taiga-ui/core";
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {TuiInputModule, TuiRadioBlockModule} from "@taiga-ui/kit";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-      TuiRootModule,
-      BrowserAnimationsModule,
-      TuiDialogModule,
-      TuiNotificationsModule
-],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        TuiRootModule,
+        TuiDialogModule,
+        TuiNotificationsModule,
+        TuiGroupModule,
+        TuiTextfieldControllerModule,
+        TuiHintControllerModule,
+        TuiButtonModule,
+        TuiInputModule,
+        TuiRadioBlockModule
+    ],
   providers: [{provide: TUI_SANITIZER, useClass: NgDompurifySanitizer}],
   bootstrap: [AppComponent]
 })
