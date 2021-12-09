7b1c7f64d554e28001c95c8e92310ee15ae2ef9a
julody <julody@mail.ru>
2021-12-09 22:08:10 +0300

    front: add client and dvd modules

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,15 +3,15 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { SearchModule } from "./search/search.module";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MainPageModule } from "./pages/main-page/main-page.module";
 
 @NgModule({
     declarations: [AppComponent],
     imports: [
+        MainPageModule,
         BrowserModule,
         AppRoutingModule,
-        SearchModule,
         BrowserAnimationsModule,
         TuiRootModule,
     ],
