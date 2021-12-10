918ef8afef1d7d459a16b342f06af06fa22aba3f
Lebedev Vladimir <58510399+GoldSwanGH@users.noreply.github.com>
2021-12-10 21:47:53 +0300

    Stored procedures added
    - added store procedures to the DB backup
    - added signatures for procedures in queries SQL file

--- database/dvd_rental_app.sql
+++ database/dvd_rental_app.sql
@@ -1,7 +1,7 @@
 ﻿--
 -- Скрипт сгенерирован Devart dbForge Studio 2020 for MySQL, Версия 9.0.567.0
 -- Домашняя страница продукта: http://www.devart.com/ru/dbforge/mysql/studio
--- Дата скрипта: 10.12.2021 19:49:49
+-- Дата скрипта: 10.12.2021 21:45:46
 -- Версия сервера: 8.0.26
 -- Версия клиента: 4.1
 --
@@ -58,7 +58,7 @@ CREATE TABLE dvd (
   UNIQUE INDEX dvd_id (dvd_id)
 )
 ENGINE = INNODB,
-AUTO_INCREMENT = 29,
+AUTO_INCREMENT = 31,
 AVG_ROW_LENGTH = 1820,
 CHARACTER SET utf8,
 COLLATE utf8_general_ci;
@@ -84,7 +84,7 @@ CREATE TABLE customers (
   UNIQUE INDEX customer_id (customer_id)
 )
 ENGINE = INNODB,
-AUTO_INCREMENT = 9,
+AUTO_INCREMENT = 11,
 AVG_ROW_LENGTH = 3276,
 CHARACTER SET utf8mb4,
 COLLATE utf8mb4_0900_ai_ci;
@@ -94,6 +94,64 @@ COLLATE utf8mb4_0900_ai_ci;
 --
 ALTER TABLE customers
 ADD UNIQUE INDEX UK_customers_tel_number (tel_number);
+
+DELIMITER $$
+
+--
+-- Создать процедуру `client_update`
+--
+CREATE
+DEFINER = 'root'@'localhost'
+PROCEDURE client_update (IN in_first_name varchar(255), IN in_last_name varchar(255), IN in_birthdate varchar(255), IN in_tel varchar(255))
+BEGIN
+  UPDATE dvd_rental_app.customers
+  SET first_name = in_first_name,
+      last_name = in_last_name,
+      birthdate = STR_TO_DATE(in_birthdate, '%d.%m.%Y')
+  WHERE tel_number = in_tel;
+END
+$$
+
+--
+-- Создать процедуру `client_find`
+--
+CREATE
+DEFINER = 'root'@'localhost'
+PROCEDURE client_find (IN find varchar(255))
+BEGIN
+  SELECT
+    *
+  FROM dvd_rental_app.customers
+  WHERE customers.tel_number LIKE CONCAT('%', find, '%');
+END
+$$
+
+--
+-- Создать процедуру `client_delete`
+--
+CREATE
+DEFINER = 'root'@'localhost'
+PROCEDURE client_delete (IN in_tel varchar(255))
+BEGIN
+  DELETE
+    FROM dvd_rental_app.customers
+  WHERE tel_number = in_tel;
+END
+$$
+
+--
+-- Создать процедуру `client_create`
+--
+CREATE
+DEFINER = 'root'@'localhost'
+PROCEDURE client_create (IN in_first_name varchar(255), IN in_last_name varchar(255), IN in_tel varchar(255), IN in_birthdate varchar(255))
+BEGIN
+  INSERT INTO dvd_rental_app.customers (first_name, last_name, tel_number, birthdate)
+    VALUES (in_first_name, in_last_name, in_tel, STR_TO_DATE(in_birthdate, '%d.%m.%Y'));
+END
+$$
+
+DELIMITER ;
 
 --
 -- Создать таблицу `rent`
@@ -108,7 +166,7 @@ CREATE TABLE rent (
   UNIQUE INDEX offer_id (offer_id)
 )
 ENGINE = INNODB,
-AUTO_INCREMENT = 12,
+AUTO_INCREMENT = 14,
 AVG_ROW_LENGTH = 5461,
 CHARACTER SET utf8,
 COLLATE utf8_general_ci;
@@ -126,6 +184,127 @@ REFERENCES customers (customer_id);
 ALTER TABLE rent
 ADD CONSTRAINT FK_offer_dvd_id FOREIGN KEY (dvd_id)
 REFERENCES dvd (dvd_id);
+
+DELIMITER $$
+
+--
+-- Создать процедуру `rent_return`
+--
+CREATE
+DEFINER = 'root'@'localhost'
+PROCEDURE rent_return (IN in_offer_id int)
+BEGIN
+  UPDATE dvd_rental_app.rent
+  SET return_date = CURRENT_TIMESTAMP()
+  WHERE offer_id = in_offer_id;
+END
+$$
+
+--
+-- Создать процедуру `rent_add`
+--
+CREATE
+DEFINER = 'root'@'localhost'
+PROCEDURE rent_add (IN in_dvd_id int, IN in_customer_id int)
+BEGIN
+  INSERT INTO dvd_rental_app.rent (dvd_id, customer_id)
+    VALUES (in_dvd_id, in_customer_id);
+END
+$$
+
+--
+-- Создать процедуру `dvd`
+--
+CREATE
+DEFINER = 'root'@'localhost'
+PROCEDURE dvd ()
+BEGIN
+  SELECT DISTINCT
+    d.*
+  FROM dvd_rental_app.dvd d
+    LEFT OUTER JOIN dvd_rental_app.rent r
+      ON d.dvd_id = r.dvd_id
+    LEFT OUTER JOIN (SELECT
+        *
+      FROM dvd_rental_app.rent
+      WHERE return_date IS NULL) re
+      ON (d.dvd_id = re.dvd_id)
+  WHERE (r.offer_id IS NULL)
+  OR (r.offer_id IS NOT NULL
+  AND re.offer_id IS NULL)
+  ORDER BY d.dvd_id DESC
+  LIMIT 5;
+END
+$$
+
+--
+-- Создать процедуру `dvd_update`
+--
+CREATE
+DEFINER = 'root'@'localhost'
+PROCEDURE dvd_update (IN in_dvd_id int, IN in_title varchar(255), IN in_year int, IN in_type_id int)
+BEGIN
+  UPDATE dvd_rental_app.dvd
+  SET title = in_title,
+      production_year = in_year,
+      type_id = in_type_id
+  WHERE dvd_id = in_dvd_id;
+END
+$$
+
+--
+-- Создать процедуру `dvd_find`
+--
+CREATE
+DEFINER = 'root'@'localhost'
+PROCEDURE dvd_find (IN find varchar(255))
+BEGIN
+  SELECT
+    *
+  FROM dvd_rental_app.dvd
+  WHERE title LIKE CONCAT('%', find, '%');
+END
+$$
+
+--
+-- Создать процедуру `dvd_delete`
+--
+CREATE
+DEFINER = 'root'@'localhost'
+PROCEDURE dvd_delete (IN id int)
+BEGIN
+  DELETE
+    FROM dvd_rental_app.dvd
+  WHERE dvd_id = id;
+END
+$$
+
+--
+-- Создать процедуру `dvd_create`
+--
+CREATE
+DEFINER = 'root'@'localhost'
+PROCEDURE dvd_create (IN in_title varchar(255), IN in_year int, IN in_type_id int)
+BEGIN
+  INSERT INTO dvd_rental_app.dvd (title, production_year, type_id)
+    VALUES (in_title, in_year, in_type_id);
+END
+$$
+
+--
+-- Создать процедуру `dvd_types`
+--
+CREATE
+DEFINER = 'root'@'localhost'
+PROCEDURE dvd_types ()
+BEGIN
+  SELECT
+    *
+  FROM dvd_rental_app.type;
+END
+$$
+
+DELIMITER ;
 
 -- 
 -- Вывод данных для таблицы type
@@ -184,13 +363,14 @@ INSERT INTO customers VALUES
 INSERT INTO rent VALUES
 (1, 1, 3, '2016-07-05 00:00:00', '2017-01-01 00:00:00'),
 (2, 7, 5, '2015-04-19 00:00:00', NULL),
-(3, 6, 1, '2021-10-07 23:57:40', '2021-12-10 18:37:30'),
+(3, 6, 1, '2021-10-07 23:57:40', '2021-12-10 20:16:00'),
 (5, 25, 3, '2021-12-10 18:51:37', '2021-12-10 18:37:30'),
 (6, 25, 4, '2021-12-10 18:51:37', NULL),
 (7, 21, 2, '2021-12-10 19:06:12', '2021-12-10 18:37:30'),
 (8, 21, 1, '2021-12-10 19:06:12', '2021-12-10 18:37:30'),
 (9, 21, 1, '2021-12-10 19:10:34', NULL),
-(11, 25, 2, '2021-12-10 19:28:52', '2021-12-10 18:37:30');
+(11, 25, 2, '2021-12-10 19:28:52', '2021-12-10 18:37:30'),
+(12, 1, 1, '2021-12-10 20:15:51', NULL);
 
 -- 
 -- Восстановить предыдущий режим SQL (SQL mode)
--- database/queries_project.sql
+++ database/queries_project.sql
@@ -1,14 +1,18 @@
--- /dvd
+﻿-- /dvd
 -- /dvd/find
 -- Поиск дисков по подстроке
 SELECT *
 FROM dvd_rental_app.dvd
 WHERE title LIKE '%find%'; -- find - подстрока, по которой ищется диск
 
+-- dvd_rental_app.dvd_find(IN find VARCHAR(255))
+
 -- /dvd/types
 -- Взять все типы дисков с их именами
 SELECT *
 FROM dvd_rental_app.type;
+
+-- dvd_rental_app.dvd_types();
 
 -- /dvd/
 -- Возвращает N самых новых и свободных dvd для отображения при открытии главной страницы
@@ -18,21 +22,29 @@ WHERE (r.offer_id IS NULL) OR (r.offer_id IS NOT NULL AND re.offer_id IS NULL)
 ORDER BY d.dvd_id DESC
 LIMIT 5;
 
+-- dvd_rental_app.dvd();
+
 -- /dvd/update
 -- Принимает данные, среди которых есть id, и устанавливает их соответствующему диску
 UPDATE dvd_rental_app.dvd
 SET title = 'test', production_year = 1990, type_id = 1
 WHERE dvd_id = 27;
 
+-- dvd_rental_app.dvd_update(IN in_dvd_id INT, IN in_title VARCHAR(255), IN in_year INT, IN in_type_id INT)
+
 -- /dvd/delete
 -- Принимает id, удаляет соответствующий диск
 DELETE FROM dvd_rental_app.dvd
 WHERE dvd_id = 28;
 
+-- dvd_rental_app.dvd_delete(IN id INT)
+
 -- /dvd/create
 -- Добавить новый диск (type_id возвращается с фронта так как мы передаем туда список типов на выбор)
 INSERT INTO dvd_rental_app.dvd (title, production_year, type_id)
 VALUES('title', 2000, 1);
+
+-- dvd_rental_app.dvd_create(IN in_title VARCHAR(255), IN in_year INT, IN in_type_id INT)
 
 -- /client
 -- /client/create
@@ -40,11 +52,15 @@ VALUES('title', 2000, 1);
 INSERT INTO dvd_rental_app.customers (first_name, last_name, tel_number, birthdate)
 VALUES('first_name', 'last_name', '+78005553535', STR_TO_DATE('01.5.2013','%d.%m.%Y'));
 
+-- dvd_rental_app.client_create(IN in_first_name VARCHAR(255), IN in_last_name VARCHAR(255), IN in_tel VARCHAR(255), IN in_birthdate VARCHAR(255))
+
 -- /client/find
 -- Найти пользователя по номеру телефона
 SELECT *
 FROM dvd_rental_app.customers
 WHERE customers.tel_number LIKE '%find%'; -- find - подстрока, по которой ищется диск
+
+-- dvd_rental_app.client_find(IN find VARCHAR(255))
 
 -- /client/update
 -- Принимает данные, среди которых есть номер телефона, и устанавливает их соответствующему клиенту
@@ -52,10 +68,14 @@ UPDATE dvd_rental_app.customers
 SET first_name = 'f', last_name = 'l', birthdate = STR_TO_DATE('01.5.2013','%d.%m.%Y')
 WHERE tel_number = '88005553535';
 
+-- dvd_rental_app.client_update(IN in_first_name VARCHAR(255), IN in_last_name VARCHAR(255), IN in_birthdate VARCHAR(255), IN in_tel VARCHAR(255))
+
 -- /client/delete
 -- Принимает номер телефона, удаляет соответствующего клиента
 DELETE FROM dvd_rental_app.customers
 WHERE tel_number = '88005553535';
+
+-- dvd_rental_app.client_delete(IN in_tel VARCHAR(255))
 
 -- /rent
 -- /rent/add
@@ -63,8 +83,12 @@ WHERE tel_number = '88005553535';
 INSERT INTO dvd_rental_app.rent (dvd_id, customer_id)
 VALUES(1, 1);
 
+-- dvd_rental_app.rent_add(IN in_dvd_id INT, IN in_customer_id INT)
+
 -- /rent/return
 -- Поставить дату возврата на запись об аренде
 UPDATE dvd_rental_app.rent
 SET return_date = CURRENT_TIMESTAMP()
 WHERE offer_id = 3;
+
+-- dvd_rental_app.rent_return(IN in_offer_id INT)
