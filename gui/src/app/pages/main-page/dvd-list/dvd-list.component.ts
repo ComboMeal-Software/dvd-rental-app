519efad342416d3530757c7161e5b201975fb89f
julody <julody@mail.ru>
2021-12-11 22:34:40 +0300

    front: replace Angular app with AngularJS app

--- gui/src/app/pages/main-page/dvd-list/dvd-list.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-    selector: "app-dvd-list",
-    templateUrl: "./dvd-list.component.html",
-    styleUrls: ["./dvd-list.component.scss"],
-})
-export class DvdListComponent {}
--- gui/src/app/pages/main-page/search-mode.ts
+++
@@ -1,4 +0,0 @@
-export enum SearchMode {
-    DVD,
-    CLIENT,
-}
