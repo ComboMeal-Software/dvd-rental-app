c6098ea89c870e2217e5050a21272ec75ed3465c
julody <julody@mail.ru>
2021-12-12 11:48:09 +0300

    front: add client adding form view

--- gui/modules/add-client-page/add-client-page.js
+++ gui/modules/add-client-page/add-client-page.js
@@ -4,4 +4,44 @@ angular.module('app.addClientPage', []).directive('appAddClientPage', [function 
     return {
         templateUrl: 'modules/add-client-page/add-client-page.html'
     };
+}]).controller('AddClientPageCtrl', ['$scope', '$http', '$timeout', function ($scope, $http, $timeout) {
+    // $scope.categoriesData = [];
+    //
+    // $scope.loadCategories = async () => {
+    //     $timeout(function () {
+    //         $scope.categoriesData = [
+    //             {
+    //                 "id": 1,
+    //                 "name": 'Film'
+    //             },
+    //             {
+    //                 "id": 2,
+    //                 "name": 'Videogame'
+    //             },
+    //             {
+    //                 "id": 3,
+    //                 "name": 'Music album'
+    //             }, {
+    //                 "id": 4,
+    //                 "name": 'TV series'
+    //             }, {
+    //                 "id": 5,
+    //                 "name": 'Animated film'
+    //             },
+    //             {
+    //                 "id": 6,
+    //                 "name": 'Animated series'
+    //             },
+    //         ]
+    //     }, 1000);
+
+        // $http.get("http://localhost:8081/lw5/main-json.jsp" + ($scope.takenOnly ? "?taken_only=true" : ""))
+        //     .then(response => {
+        //         $scope.showLoader = false;
+        //         $scope.dvdData = response.data;
+        //     });
+    // }
+
+    // $scope.loadCategories();
 }]);
+
