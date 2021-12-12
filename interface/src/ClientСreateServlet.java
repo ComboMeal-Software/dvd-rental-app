8fd70e7d8e0ffc6c3480567b2794a25241c747c7
Dmitry Milya <dmilya2000@gmail.com>
2021-12-12 13:30:20 +0300

    Добавлен сервлет для /client/create

---
+++ interface/src/ClientСreateServlet.java
@@ -0,0 +1,48 @@
+package ml.thomasmiles.dvdrentalapp;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+
+@WebServlet(name = "ClientCreateServlet", value = "/client/create")
+public class ClientCreateServlet extends HttpServlet {
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
+    private static void queryMessages(Connection connection, String firstName, String lastName, String telNumber,
+                                      String birthDate)
+            throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement("CALL client_create(?, ?, ?, ?);");
+        preparedStatement.setString(1, firstName);
+        preparedStatement.setString(2, lastName);
+        preparedStatement.setString(3, telNumber);
+        preparedStatement.setString(4, birthDate);
+
+        preparedStatement.executeQuery();
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        ClientDto clientDto = mapper.readValue(request.getInputStream(), ClientDto.class);
+
+        try {
+            String dbUrl = "jdbc:mysql://localhost/dvd_rental_app?useSSL=false";
+
+            try (Connection connection = DriverManager.getConnection(dbUrl, USERNAME, PASSWORD)) {
+                queryMessages(connection, clientDto.getFirstName(), clientDto.getLastName(), clientDto.getTelNumber(),
+                        clientDto.getBirthDate());
+            }
+        } catch (Exception ex) {
+            writer.println(ex);
+        } finally {
+            writer.close();
+        }
+    }
+}
