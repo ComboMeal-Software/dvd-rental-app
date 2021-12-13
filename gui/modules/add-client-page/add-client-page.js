e72f3afbf338c2d58b2263de9a17650e7a93a8a1
julody <julody@mail.ru>
2021-12-13 17:25:16 +0300

    Code style

--- gui/modules/add-client-page/add-client-page.js
+++ gui/modules/add-client-page/add-client-page.js
@@ -4,22 +4,23 @@ angular.module('app.addClientPage', []).directive('appAddClientPage', [function 
     return {
         templateUrl: 'modules/add-client-page/add-client-page.html'
     };
-}]).controller('AddClientPageCtrl', ['$scope', '$http', '$location', '$window', function ($scope, $http, $location, $window) {
-    $scope.baseUrl = new $window.URL($location.absUrl()).origin;
+}]).controller('AddClientPageCtrl', ['$scope', '$http', '$location', '$window',
+    function ($scope, $http, $location, $window) {
+        $scope.baseUrl = new $window.URL($location.absUrl()).origin;
 
-    $scope.client = {};
+        $scope.client = {};
 
-    $scope.submitForm = () => {
-        $http({
-            method: 'POST',
-            url: $scope.baseUrl + '/dvd-rental-app/client/create',
-            data: $scope.client,
-        }).then(() => {
-            alert('Success!');
-            $location.path("main");
-        }).catch(() => {
-            alert('Error was occurred!');
-        });
-    };
-}]);
-
+        $scope.submitForm = () => {
+            $http({
+                method: 'POST',
+                url: $scope.baseUrl + '/dvd-rental-app/client/create',
+                data: $scope.client,
+            }).then(() => {
+                alert('Success!');
+                $location.path("main");
+            }).catch(() => {
+                alert('Error was occurred!');
+            });
+        };
+    }]
+);
